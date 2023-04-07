85947337c2ee712f9f0cfcb39c1fa8f710440010
Dzmitry Lukashuk <dmitry.lukashuk@sobrado.ch>
2023-04-07 22:42:57 +0200

    Added task 3

--- lesson1.py
+++ lesson1.py
@@ -19,6 +19,16 @@ def task_4():
     print(f"Result: {peter_1_x} {kate_4_x} {serge_1_x}")
 
 
+def task_6():
+    num = str(input('Enter 6 digit number: '))
+    first_part = sum_of_numbers(num[0:3])
+    second_part = sum_of_numbers(num[3:6])
+    if first_part == second_part:
+        print('yes')
+    else:
+        print('no')
+
+
 task_2()
 task_4()
-# task_6()
+task_6()
